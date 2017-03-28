c34163ccabf4b969ed81770a0d6dee1bb59a0bef
Ilia I <ilya.pskov@gmail.com>
2017-03-28 22:07:18 +0300

    Cachier refactored. Readme updated. Exposed additional functions.
    * Examples added. Refactoring cachier.
    
    * Readme updated, cachier refactored.
    
    * Readme type.
    
    * Readme update.
    
    * Unnecessary stuff removed.

---
+++ example/example.js
@@ -0,0 +1,23 @@
+const tsp = require('./../index.js');
+
+// tsp.cachier.loadCachedFiles(true).then((data) => {
+// 	console.log(data);
+// }, (error) => {
+// 	console.log(error);
+// });
+
+ tsp.getCachedData().then((data) => {
+     console.log(data.stops);
+	// console.log(`Stops: ${data.stops.length}`);
+	// console.log(`Routes: ${data.routes.length}`);
+	// console.log(`Gps: ${data.gps.length}`);
+}, (error) => {
+	console.log(error);
+});
+
+
+// tsp.cachier.validateFilesExists().then((data) => {
+//    console.log(data);
+// }, (error) => {
+// 	console.log(error);
+// });
